feaf5fb731798257fd47210e24f72b9c5e06da27
ASeR77795 <109546726+ASeR77795@users.noreply.github.com>
2022-10-11 15:45:37 +0300

    hw02

---
+++ front-end-pro-01/homework-02/index.js
@@ -0,0 +1,5 @@
+var num1 = prompt("первое число");
+var num2 = prompt("второе число");
+var num3 = prompt("третье число");
+var text = "Среднее арефметическое чисел" + +num1 + num2 + num3 + "=";
+alert(text + (+num1 + +num2 + +num3) / 3);
