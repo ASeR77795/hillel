cd4644775eb1e2bd8ab3e3e0e2e28da76c7a4eb3
Pavel Fedorenko <winterspray@gmail.com>
2022-10-12 19:18:54 +0300

    hw04

---
+++ front-end-pro-01/homework-04/index.js
@@ -0,0 +1,16 @@
+let name = prompt("Enter your name!");
+let age = prompt("Enter your age!");
+let surName = prompt("Enter your surname!");
+alert(
+  "Ваше имя - " +
+    name +
+    "\n" +
+    "Ваш возраст - " +
+    age +
+    "\n" +
+    "Ваша фамилия - " +
+    surName
+);
+
+const x = 12345;
+console.log(String(x).split("").join(" "));
