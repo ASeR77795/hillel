6b458731bcd2afc413b654bee887af6d975bee1c
Pavel Fedorenko <winterspray@gmail.com>
2022-10-13 00:10:35 +0300

    hw07

---
+++ front-end-pro-01/homework-07/index.js
@@ -0,0 +1,12 @@
+var want = prompt("что сделать:\nadd, sub, mult, div");
+var num1 = prompt("первое число");
+var num2 = prompt("второе число");
+if (want === "add") {
+  alert(num1 + " + " + num2 + " = " + (Number(num1) + Number(num2)));
+} else if (want === "sub") {
+  alert(num1 + " - " + num2 + " = " + (Number(num1) - Number(num2)));
+} else if (want === "mult") {
+  alert(num1 + " * " + num2 + " = " + Number(num1) * Number(num2));
+} else if (want === "div") {
+  alert(num1 + " / " + num2 + " = " + Number(num1) / Number(num2));
+}
