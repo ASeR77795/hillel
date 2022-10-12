f15132b9a50c7884769b25ffb01da767ae13f6b9
Pavel Fedorenko <winterspray@gmail.com>
2022-10-12 23:18:51 +0300

    hw06

--- front-end-pro-01/homework-06/index.js
+++ front-end-pro-01/homework-06/index.js
@@ -1,2 +1,38 @@
-let num = prompt();
-console.log({ num });
+let num1 = prompt("первое число");
+let num2 = prompt("второе число");
+let sum = Number(num1) + Number(num2);
+let sub = Number(num1) - Number(num2);
+let mult = Number(num1) * Number(num2);
+let div = Number(num1) / Number(num2);
+alert(
+  "Пользователь ввел " +
+    num1 +
+    " и " +
+    num2 +
+    " : " +
+    "\n" +
+    num1 +
+    "+" +
+    num2 +
+    "=" +
+    sum +
+    "\n" +
+    num1 +
+    "-" +
+    num2 +
+    "=" +
+    sub +
+    "\n" +
+    num1 +
+    "*" +
+    num2 +
+    "=" +
+    mult +
+    "\n" +
+    num1 +
+    "/" +
+    num2 +
+    "=" +
+    div +
+    "\n"
+);
