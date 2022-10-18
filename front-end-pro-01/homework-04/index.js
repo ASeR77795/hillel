0f22c405195977f04a290e5f4a4e8442f31b8624
Pavel Fedorenko <winterspray@gmail.com>
2022-10-18 18:15:39 +0300

    fix incorect imput

--- front-end-pro-01/homework-04/index.js
+++ front-end-pro-01/homework-04/index.js
@@ -1,16 +1,20 @@
 let name = prompt("Enter your name!");
-let age = prompt("Enter your age!");
+let age = +prompt("Enter your age!");
 let surName = prompt("Enter your surname!");
-alert(
-  "Ваше имя - " +
-    name +
-    "\n" +
-    "Ваш возраст - " +
-    age +
-    "\n" +
-    "Ваша фамилия - " +
-    surName
-);
+if (isNaN(name) && !isNaN(age) && isNaN(surName)) {
+  alert(
+    "Ваше имя - " +
+      name +
+      "\n" +
+      "Ваш возраст - " +
+      age +
+      "\n" +
+      "Ваша фамилия - " +
+      surName
+  );
+} else {
+  alert("Incorect imput!");
+}
 
 const x = 12345;
 console.log(String(x).split("").join(" "));
