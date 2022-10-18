c2753d8ea80b06d880f955a6d4cae8b7cf7f1c23
Pavel Fedorenko <winterspray@gmail.com>
2022-10-18 18:10:30 +0300

    fix isNaN

--- front-end-pro-01/homework-02/index.js
+++ front-end-pro-01/homework-02/index.js
@@ -1,11 +1,19 @@
-let num1 = prompt("первое число");
-let num2 = prompt("второе число");
-let num3 = prompt("третье число");
-let text = "Среднее арефметическое чисел" + +num1 + num2 + num3 + "=";
-if (isNaN(num1) && isNaN(num2) && isNaN(num3)) {
+let num1 = +prompt("первое число");
+let num2 = +prompt("второе число");
+let num3 = +prompt("третье число");
+let text =
+  "Среднее арефметическое чисел: " +
+  num1 +
+  ", " +
+  num2 +
+  ", " +
+  num3 +
+  ", " +
+  " = ";
+if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
   alert("Incorect imput!");
 } else if (num1 === "" || num2 === "" || num3 === "") {
   alert("Incorect imput!");
 } else {
-  alert(text + (+num1 + +num2 + +num3) / 3);
+  alert(text + (num1 + num2 + num3) / 3);
 }
