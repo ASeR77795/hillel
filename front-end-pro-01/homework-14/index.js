28ae771410d91fba0b3f0023339934719eb5a7d3
Pavel Fedorenko <winterspray@gmail.com>
2022-10-29 18:41:57 +0300

    dome hw14

---
+++ front-end-pro-01/homework-14/index.js
@@ -0,0 +1,47 @@
+// Створити об'єкт, та при його створенні відразу зробити декілька властивостей(свойств)
+
+// Назовіть об'єкт з назвою store.
+
+// При створенні зробіть відразу властивості nameі запишіть у нього назву свого уявного магазину.
+
+// Також створіть властивість address, який буде об'єктом і
+// там запишіть властивості country, city, street, houseNumber з адресою вашого уявного магазину.
+
+// Потім створіть окремо(тобто не при ініціалізації,
+//   а нижче(ініацілізація це процес сворення та коли ми відразу туди сетемо якісь властивості)) властивість clients,
+//    і значенням буде пустий об'єкт.
+
+// Потім до clients зробіть 5 властивостей, які будуть називатися іменами і
+// прізвищами 5 рандомних учасників нашого курсу(мене також можно) і
+// у кожного має бути значення з кількіст'ю покупок(quantity) та кількість бонусів(bonuses). Ці значення будуть числами
+
+let store = {
+  name: "Beautiful",
+  adress: {
+    country: "Ukraine",
+    city: "Odessa",
+    street: "Balkivska",
+    houseNumber: 30,
+  },
+};
+store.clients = {};
+store.clients["Irina Mararova"] = {
+  quantity: Number(),
+  bonuses: Number(),
+};
+store.clients["Mikhall Hitko"] = {
+  quantity: Number(),
+  bonuses: Number(),
+};
+store.clients["Oleksandra Horbenko"] = {
+  quantity: Number(),
+  bonuses: Number(),
+};
+store.clients["Olena Demchuk"] = {
+  quantity: Number(),
+  bonuses: Number(),
+};
+store.clients["Pavlo Fedorenko"] = {
+  quantity: Number(),
+  bonuses: Number(),
+};
